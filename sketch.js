eb371639925098e53672ea14f2b088109e9e732b
Sarah Lede <sml19f@fsu.edu>
2022-11-07 20:21:01 -0500

    instructionsaddand.56speed

--- sketch.js
+++ sketch.js
@@ -1,6 +1,6 @@
 let percent = 0;
 let smallestSize = 100;
-let speed = .7;
+let speed = .56;
 let siz;
 
 function setup() {
@@ -9,10 +9,32 @@ function setup() {
 }
 
 function draw() {
-  background(125);
+  background('light blue');
 
   siz = map2(percent, 0, 100, smallestSize*5, 100*5, SINUSOIDAL, BOTH);
-  ellipse(width/2, height/2, siz, siz);
+    // noStroke();
+    let from = color(218, 165, 32);
+    let to = color(72, 61, 139);
+    let gradient = lerpColor(from, to, sin(frameCount * 0.005));
+    fill(gradient);
+    ellipse(width/2, height/2, siz, siz);
+ noFill()
+  stroke(100, 100, 240);
+  // rect(230, 390, 440, 150);
+
+  textSize(24);
+  text('Welcome to the 20% breathe excercise', 40, 320);
+
+  textSize(16);
+  text('1. Inhale a deep breath, this will put your circle at 100%', 40, 370);
+  text('2. Slowly begin to exhale in 20% increments', 40, 420);
+
+  textSize(10);
+  text('(Ex: your first exhale will leave your circle at 80% and last will be 0%)', 40, 435);
+
+  textSize(16);
+  text('3. After every 20% exhale, inhale back to 100%', 40, 480);
+  text('4. Repeat until calmer...', 40, 520);
 
   percent += speed;
   if (percent >= 100) { // got to the largest size
